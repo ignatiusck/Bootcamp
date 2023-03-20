c46b7351e7732c43fb2a0aae614bbee20bcac0ab
ignatius chandra <ignatius.c.k@gmail.com>
2023-03-20 21:38:53 +0700

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main() {
+        Cat memong = new Cat();
+
+        memong.meow();
+        Console.WriteLine(memong.foodCount<int>(2));
+    }
+
+    abstract class Animal
+    {
+        public string name;
+
+        public Animal () {
+            name = "none";
+        }
+
+        public virtual void meow() {
+            Console.WriteLine("okeh");
+        }
+
+        public abstract DT foodCount<DT> (DT food);
+    }
+
+    class Cat : Animal
+    {
+        public override void meow () {
+            Console.WriteLine("meow");
+        }
+
+        public override DT foodCount<DT> (DT food) {
+            return food;
+        }
+    }
+}
