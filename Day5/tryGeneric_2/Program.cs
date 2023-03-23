511dccadfd288a1f26917d4e07e4561c12ba656a
Ignatius Chandra Kurniawan <ignatius.c.k@gmail.com>
2023-03-23 12:25:23 +0700

    Day 5 done

---
+++ Day5/tryGeneric_2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Namespace
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Car? red = new();
+            Console.WriteLine($"{red.name}, {red.speed}");
+
+            red = EditCar.EditDataCar(red, "yellow");
+            Console.WriteLine($"{red.name}, {red.speed}");
+        }
+
+        internal class Car
+        {
+            internal string? name;
+            internal int? speed;
+
+            internal Car()
+            {
+                name = "none";
+                speed = 0;
+            }
+        }
+
+        internal class EditCar
+        {
+            internal string? name;
+            internal int? speed;
+            internal EditCar()
+            {
+                name = "none";
+                speed = 0;
+            }
+            internal static Car? EditDataCar<KC, K1>(KC Cname, K1 Name)
+            {
+                if (Cname is Car newCname && Name is string newName)
+                {
+                    newCname.name = newName;
+                    return newCname;
+                }
+                return null;
+            }
+        }
+    }
+}
