37943789807c1dd2ecfc23f9807b435fd9f46d7c
Ignatius Chandra Kurniawan <ignatius.c.k@gmail.com>
2023-03-30 07:59:29 +0700

    update

---
+++ Day10/tryCollections/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+class Program
+{
+    public static void Main()
+    {
+        Dictionary<int, string> dict = new();
+
+        ArrayList arr = new();
+        arr.RemoveAt(2);
+    }
+}
